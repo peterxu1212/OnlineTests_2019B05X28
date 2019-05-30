3c98f861879803d3a70a8505c7e08939e739f776
peterxu1212 <peterxu1212@gmail.com>
2019-05-30 04:49:04 -0400

    commit 5
     for spring boot web app which supports gravata image url now

--- src/main/java/com/acquisio/app/controller/EmployeeController.java
+++ src/main/java/com/acquisio/app/controller/EmployeeController.java
@@ -13,6 +13,14 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+
+/* The following results returned from http://localhost:8080/employees, so can use gravatarImageURL to find the gravatar image now
+
+[{"id":1,"firstName":"John","lastName":"Smith","email":"onlinetest.acquisio+jsmith@gmail.com","salary":20000,"gravatarImageURL":"https://www.gravatar.com/avatar/a2d0c93c17683377100645a0d1db3d2f"},{"id":2,"firstName":"Ava","lastName":"Muffinson","email":"onlinetest.acquisio+amuffinson@gmail.com","salary":10000,"gravatarImageURL":"https://www.gravatar.com/avatar/a25a90b8bbe6acb55c4d7ac5007894e1"},{"id":3,"firstName":"Cailin","lastName":"Ninson","email":"onlinetest.acquisio+cninson@gmail.com","salary":30000,"gravatarImageURL":"https://www.gravatar.com/avatar/6838aa212ebe3786c73696ede0c21d4f"},{"id":4,"firstName":"Mike","lastName":"Peterson","email":"onlinetest.acquisio+mpeterson@gmail.com","salary":20000,"gravatarImageURL":"https://www.gravatar.com/avatar/bf6fc85076d59cc375fa0520a6b60fab"},{"id":5,"firstName":"Ian","lastName":"Peterson","email":"onlinetest.acquisio+ipeterson@gmail.com","salary":80000,"gravatarImageURL":"https://www.gravatar.com/avatar/4fd6503f7b75a5eb244c98a2107ddfe7"},{"id":6,"firstName":"John","lastName":"Mills","email":"onlinetest.acquisio+jmills@gmail.com","salary":50000,"gravatarImageURL":"https://www.gravatar.com/avatar/29a7feb1240230c92509e18115bb179e"}]
+
+*/
+
+
 @RestController
 public class EmployeeController {
 
--- src/main/java/com/acquisio/app/domain/Employee.java
+++ src/main/java/com/acquisio/app/domain/Employee.java
@@ -5,6 +5,14 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+
+import java.util.*;
+import java.io.*;
+import java.security.*;
+
+
+
+
 @Entity
 public class Employee {
 
@@ -18,6 +26,16 @@ public class Employee {
     @Column(name = "email")
     private String email;
     private int salary;
+	
+	/*
+	@Column(name = "email_md5_hash")
+	private String emailMd5Hash;
+	*/
+	
+	@Column(name = "gravatar_image_url")
+	private String gravatarImageURL;
+	
+	
 
     Employee() {
         // JPA only
@@ -28,6 +46,10 @@ public class Employee {
         this.lastName = lastName;
         this.salary = salary;
         this.email = email;
+		
+		//this.emailMd5Hash = md5Hex(email.toLowerCase());
+		this.gravatarImageURL = "https://www.gravatar.com/avatar/" + md5Hex(email.toLowerCase());
+		
     }
 
     public Long getId() {
@@ -69,4 +91,46 @@ public class Employee {
     public void setEmail(String email) {
         this.email = email;
     }
+	
+	/*
+	public String getEmailMd5Hash() {
+        return emailMd5Hash;
+    }
+
+    public void setEmailMd5Hash(String emailMd5Hash) {
+        this.emailMd5Hash = emailMd5Hash;
+    }
+	*/
+	
+	public String getGravatarImageURL() {
+        return gravatarImageURL;
+    }
+
+    public void setGravatarImageURL(String gravatarImageURL) {
+        this.gravatarImageURL = gravatarImageURL;
+    }
+	
+	
+	
+	//https://en.gravatar.com/site/implement/images/java/
+	public static String hex(byte[] array) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < array.length; ++i) {
+		sb.append(Integer.toHexString((array[i]
+		  & 0xFF) | 0x100).substring(1,3));        
+		}
+		return sb.toString();
+	}
+	
+	public static String md5Hex (String message) {
+		try {
+		MessageDigest md = 
+		  MessageDigest.getInstance("MD5");
+		return hex (md.digest(message.getBytes("CP1252")));
+		} catch (NoSuchAlgorithmException e) {
+		} catch (UnsupportedEncodingException e) {
+		}
+		return null;
+	}
+	
 }
